e27c5f17a7526ea55107d41721306bc293857746
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-02-04 13:19:09 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 import { CTA, Brand, Navbar } from './components'
+import './App.css'
 
 const App = () => {
   return (
